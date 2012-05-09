9e91a78222793e34c1d2c5c46db48744ec1a944c
Ryan Brainard <ryanbrainard@gmail.com>
2012-05-09 11:25:03 -0700

    increase scope to include private repos

--- app/services/GithubApi.scala
+++ app/services/GithubApi.scala
@@ -16,7 +16,7 @@ object GitHubApi extends OAuthService {
   private val clientId = sys.env.getOrElse("GITHUB_CLIENT_ID", sys.error("GITHUB_CLIENT_ID not configured"))
   private val clientSecret = sys.env.getOrElse("GITHUB_CLIENT_SECRET", sys.error("GITHUB_CLIENT_SECRET not configured"))
 
-  def userAuthUrl =  "https://github.com/login/oauth/authorize?client_id=%s".format(clientId)
+  def userAuthUrl =  "https://github.com/login/oauth/authorize?client_id=%s&scope=repo".format(clientId)
 
   def exchangeCodeForAccessToken(code: String): Promise[String] = {
     val accessTokenExchangeResponsePattern = """.*access_token=(\w+).*""".r
